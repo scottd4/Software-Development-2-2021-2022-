40152478fa0dcb4dee39b55cb66ce24b82f2b1fd
davidscott <davidscott@ait.ie>
2021-09-30 16:08:48 +0100

    Added W203

---
+++ InClass160921/src/Box.java
@@ -0,0 +1,38 @@
+public class Box {
+
+    private boolean full = false;
+    private String boxName;
+
+    public Box()
+    {
+        boxName = "cardboard";
+    }
+
+    public void setFull(boolean full)
+    {
+        this.full= full;
+    }
+
+    public String getFull()
+    {
+        return full ? "Full" : "Empty";
+    }
+
+    public String toString()
+    {
+        return "Box type : " + this.boxName +
+                "\nBox Status: "
+                + this.getFull();
+    }
+
+    public static void main(String[] args) {
+
+        Box b = new Box();
+        b.setFull(true);
+        System.out.println(b);
+
+    }
+
+
+
+}
