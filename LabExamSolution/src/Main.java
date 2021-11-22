c5e49e7b7aaf576b6e348f0734ccd483ae49243b
davidscott <davidscott@ait.ie>
2021-11-22 13:32:08 +0000

    lab exam

---
+++ LabExamSolution/src/Main.java
@@ -0,0 +1,78 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void readFile(String filename)
+    {
+        ArrayList<ParkStat> parks =  new ArrayList<>();
+
+        try{
+            Scanner sc = new Scanner(new File(filename));
+
+            if(sc.hasNextLine())
+            {
+                sc.nextLine(); //skip the headings
+            }
+
+            while (sc.hasNextLine())
+            {
+                parks.add(new ParkStat(sc.nextLine()));
+            }
+
+            //sort
+            parks.sort(null);
+
+            for (ParkStat p : parks)
+            {
+                System.out.println(p);
+            }
+
+            ParkStat.sortOrder = 1;
+
+            parks.sort(null);
+
+            System.out.println("\n\nSort order changed\n");
+
+            printParkDetails(parks);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static void printParkDetails(ArrayList<ParkStat> parks)
+    {
+        String type = null;
+        int count = 0;
+
+        for (ParkStat p : parks)
+        {
+            if (type == null)
+            {
+                type = p.getType();
+                count++;
+            }
+            else if(type.equals(p.getType()))
+            {
+                count++;
+            }
+            else
+            {
+                System.out.println("Type: " + type + " count " + count );
+                type = p.getType();
+                count = 1;
+
+            }
+        }
+        System.out.println("Type: " + type + " count " + count );
+
+
+    }
+
+    public static void main(String[] args) {
+        readFile("labexam.txt");
+    }
+}
---
+++ LabExamSolution/src/ParkStat.java
@@ -0,0 +1,70 @@
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class ParkStat implements Comparable<ParkStat> {
+
+    private String name,address,website,type,muga;
+
+    public static int sortOrder = 0;
+
+    public ParkStat(String s)
+    {
+        Scanner sc = new Scanner(s);
+        sc.useDelimiter(",");
+
+        sc.next();//dont care about id
+
+        if(s.contains("\""))
+        {
+            name = extractDetails(sc.next(), sc);
+            address = extractDetails(sc.next(), sc);
+
+            website = sc.next();
+            type = sc.next();
+            muga = sc.next();
+
+        }
+        else
+        {
+            name = sc.next();
+            address = sc.next();
+            website = sc.next();
+            type = sc.next();
+            muga = sc.next();
+        }
+    }
+
+    public String extractDetails(String s, Scanner sc)
+    {
+        if(s.contains("\""))
+        {
+            sc.useDelimiter("\"");
+
+            s = s.replace("\"", "")+ sc.next();
+
+            sc.useDelimiter(",");
+            sc.next();// skips the "
+        }
+
+        return s;
+    }
+
+    public String toString()
+    {
+        return name + ", " + address + ", " +website + ", " +type + ", " + muga;
+    }
+
+    public String getType()
+    {
+        return type;
+    }
+
+    @Override
+    public int compareTo(ParkStat o) {
+        if (sortOrder == 1)
+        {
+            return this.type.compareTo(o.type);
+        }
+        return this.name.compareTo(o.name);
+    }
+}
