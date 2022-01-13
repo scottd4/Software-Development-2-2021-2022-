fb373f5dc3cec3097e6e3ade65dbda233d031ae8
davidscott <davidscott@ait.ie>
2022-01-13 10:02:52 +0000

    Lab 7 addition

--- InClass110122/src/CurrencyConvert.java
+++ InClass110122/src/CurrencyConvert.java
@@ -1,6 +1,16 @@
 public class CurrencyConvert implements CurrencyInterface {
 
-    private double usd =0.0;
+    private double usd = 0.0;
+
+    @Override
+    public double getUSD() {
+        return usd;
+    }
+
+    @Override
+    public void setUSD(double value) {
+        usd = value;
+    }
 
     @Override
     public void euroToUSD(double value) {
@@ -34,5 +44,17 @@ public class CurrencyConvert implements CurrencyInterface {
         return 0;
     }
 
+    public static void main(String[] args) {
+        CurrencyConvert c = new CurrencyConvert();
+
+        c.euroToUSD(50);
+
+        System.out.println("The value in USD is: " + c.getUSD());
+
+        c.setUSD(1000);
+
+        System.out.println("The value in Euro is: " + c.usdToEuro());
+
+    }
 
 }
--- InClass110122/src/CurrencyInterface.java
+++ InClass110122/src/CurrencyInterface.java
@@ -1,5 +1,8 @@
 public interface CurrencyInterface {
 
+    double getUSD();
+    void setUSD(double value);
+
     void euroToUSD(double value);
     double usdToEuro();
 
