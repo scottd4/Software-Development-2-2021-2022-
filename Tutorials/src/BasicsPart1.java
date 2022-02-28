8d8dbd68fc2d9051a27379dd24fcbbc4e76cdf9d
scottd4 <david.scott4@gmail.com>
2022-02-28 09:36:04 +0000

    tutorial basics

---
+++ Tutorials/src/BasicsPart1.java
@@ -0,0 +1,94 @@
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+public class BasicsPart1 {
+
+    public static void main(String[] args) {
+
+
+        // Primitives
+        // bolean b = true;
+        Boolean b = new Boolean(true);
+
+        // Strring s = "hello World";
+        String s = new String("Hello World");
+
+        // int i  = 10;
+        Integer i = new Integer(10);
+
+        i++;
+
+        // Loops
+
+        //while loop
+
+        int count = 0;
+
+        while(count < 10)
+        {
+            System.out.println("While" + count);
+            count++;
+        }
+
+        count = 0;
+        // do while
+
+        do {
+            System.out.println("do while " + count);
+            count++;
+        }while (count < 10);
+
+
+        // for loops
+
+        int [] a = new int[10];
+
+        for (i = 0; i < a.length; i++)
+        {
+           a[i] = i;
+
+        }
+
+        // foreach
+
+        for(int element : a)
+        {
+            System.out.println("foreach " + element);
+        }
+
+        //Continue Break
+
+        for (i = 0; i < 10; i++)
+        {
+            for (int j = 0; j<10; j++)
+            {
+                //System.out.print("*");
+
+                if (j == i )
+                {
+                    break;
+                }
+            }
+            System.out.println();
+        }
+
+
+
+        // Exceptions
+
+        try{
+            System.out.println(a[11]);
+        }
+        catch (ArrayIndexOutOfBoundsException e)
+        {
+            System.out.println("Array too small");
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally {
+            //cleanup memory here
+        }
+
+    }
+}
